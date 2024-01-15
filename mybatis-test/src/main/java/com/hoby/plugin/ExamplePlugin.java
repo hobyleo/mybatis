9b69a64a6e3301bab5cbc725769960e168a0d98f
hoby <m1ch3ng@gmail.com>
2024-01-15 15:44:53 +0800

    plugin usage

--- mybatis-test/src/main/java/com/hoby/plugin/ExamplePlugin.java
+++ mybatis-test/src/main/java/com/hoby/plugin/ExamplePlugin.java
@@ -2,12 +2,13 @@ package com.hoby.plugin;
 
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.plugin.Interceptor;
-import org.apache.ibatis.plugin.Intercepts;
-import org.apache.ibatis.plugin.Invocation;
-import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.plugin.*;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Properties;
 
 /**
  * @author hoby
@@ -18,11 +19,50 @@ import org.apache.ibatis.session.RowBounds;
 })})
 public class ExamplePlugin implements Interceptor {
 
+    Properties properties = null;
+
+    /**
+     * 拦截方法逻辑
+     * 这里主要是通过反射去获取要执行的SQL相关信息，然后进行操作
+     */
     public Object intercept(Invocation invocation) throws Throwable {
-        System.out.println("代理");
-        Object[] args = invocation.getArgs();
-        MappedStatement ms = (MappedStatement) args[0];
-        return invocation.proceed();
+        Object target = invocation.getTarget(); // 被代理对象
+        Method method = invocation.getMethod(); // 代理方法
+        Object[] args = invocation.getArgs(); // 方法参数
+
+        // do something ... 方法拦截前执行代码块
+        System.out.println("target = " + target);
+        System.out.println("method = " + method);
+        System.out.println("args = " + Arrays.toString(args));
+        System.out.println("properties = " + properties);
+
+        Object result = invocation.proceed();
+
+        // do something ... 方法拦截后执行代码块
+        System.out.println("result = " + result);
+
+        return result;
+    }
+
+    /**
+     * 生成MyBatis拦截器代理对象
+     */
+    @Override
+    public Object plugin(Object target) {
+        if (target instanceof Executor) {
+            // 调用插件
+            return Plugin.wrap(target, this);
+        }
+        return target;
+    }
+
+    /**
+     * 设置插件属性（直接通过Spring的方式获取属性，所以这个方法一般也用不到）
+     * 项目启动的时候数据就会被加载
+     */
+    @Override
+    public void setProperties(Properties properties) {
+        this.properties = properties;
     }
 
 }
